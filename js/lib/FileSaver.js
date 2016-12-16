e487899e8e49c8c87d9408e57a288304af0775ab
clementzheng <clement.zheng@gmail.com>
2016-12-16 18:04:13 -0500

    update

--- js/lib/FileSaver.js
+++ js/lib/FileSaver.js
@@ -182,7 +182,7 @@ var saveAs = saveAs || (function(view) {
 if (typeof module !== "undefined" && module.exports) {
   module.exports.saveAs = saveAs;
 } else if ((typeof define !== "undefined" && define !== null) && (define.amd !== null)) {
-  define("FileSaver.js", function() {
+  define([], function() {
     return saveAs;
   });
 }
